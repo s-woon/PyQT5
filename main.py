e4aa2b4400cb4a6e9776752487b3ab32f9a326b4
s-woon <sus9763@gmail.com>
2022-02-21 14:37:05 +0900

    finish test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import numpy as np
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
@@ -8,12 +9,17 @@ from face import facecam
 from edge import Edge
 from rgb import rgb
 from movecam import Move
+from rgbvideo import rgbvideo
 from affine import Affine
 
 
 class Thread(QThread):
     changePixmap = pyqtSignal(QImage)
+    recordFrame = pyqtSignal(np.ndarray)
+    Frame = pyqtSignal(np.ndarray)
+
     # print('1')
+
 
     def run(self):
         print('카메라실행')
@@ -21,6 +27,7 @@ class Thread(QThread):
         cap = cv2.VideoCapture(0)
         while True:
             ret, frame = cap.read()
+            self.recordFrame.emit(frame)
             if ret:
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 rgbImage = cv2.flip(rgbImage, 1) # 좌우반전
@@ -29,6 +36,7 @@ class Thread(QThread):
                 cvc = QImage(rgbImage.data, w, h, bytePerLine, QImage.Format_RGB888)
                 p = cvc.scaled(640, 480, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
+                self.Frame.emit(rgbImage)
                 # print('3')
 
 
@@ -43,13 +51,19 @@ class WindowClass(QMainWindow):
         self.actionrgb.triggered.connect(self.gorgb)
         self.actionmove.triggered.connect(self.gomove)
         self.actionedges.triggered.connect(self.goedge)
+        self.actionrgbvideo.triggered.connect(self.gorgbvideo)
         # self.actionaffine.triggered.connect(self.goaffine)
 
         self.stackedWidget.insertWidget(1, facecam(self))
         self.stackedWidget.insertWidget(2, Edge(self, self.th))
         self.stackedWidget.insertWidget(3, rgb(self, self.th))
         self.stackedWidget.insertWidget(4, Move(self, self.th))
+        self.stackedWidget.insertWidget(5, rgbvideo(self, self.th))
         # self.stackedWidget.insertWidget(3, Affine(self, self.th))
+
+        self.startbtn.setCheckable(True)
+        self.startbtn.clicked.connect(self.recstart)
+
 
     def setImage(self, image): # image = p
         # lbW = self.cameraLb.width()
@@ -63,40 +77,25 @@ class WindowClass(QMainWindow):
         self.th.start()
         # print('2')
 
-    def record(self):
-        print('Recording Start')
-        fps = 29.97
-        print(1)
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        print(2)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        print(3)
-        delay = round(1000/fps)
-        print(4)
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        print(5)
-        writer = cv2.VideoWriter('test.avi', fourcc, fps, size)
-        print(6)
-        if writer.isOpened() == False: raise Exception("동영상 파일 개방 안됨")
-        print(7)
-        while True:
-            print(8)
-            ret, frame = cap.read()
-            print(9)
-            if not ret: break
-            print(10)
-            if cv2.waitKey(delay) >= 0: break
-            print(11)
+    def recstart(self, state):
+        global writer
+        if state:
+            fps = 29.97
+            size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            delay = round(1000 / fps)
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            writer = cv2.VideoWriter('test.avi', fourcc, fps, size)
+            self.th.recordFrame.connect(self.recording)
+        else:
+            writer.release()
+            self.th.recordFrame.disconnect(self.recording)
+            print('Recording Stop')
 
-            writer.write(frame)
-            print(12)
-
-        writer.release()
-        print(13)
-        cap.release()
-        print('Recording Stop')
-
+    def recording(self, frame):
+        writer.write(frame)
+        print('Recording.')
+        print('Recording..')
+        print('Recording...')
 
     def gobackhome(self):
         self.stackedWidget.setCurrentIndex(0)
@@ -112,6 +111,9 @@ class WindowClass(QMainWindow):
 
     def gomove(self):
         self.stackedWidget.setCurrentIndex(4)
+
+    def gorgbvideo(self):
+        self.stackedWidget.setCurrentIndex(5)
 
     # def goaffine(self):
     #     self.stackedWidget.setCurrentIndex(3)
--- rgbvideo.py
+++ rgbvideo.py
@@ -7,21 +7,24 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtGui
 
 
-class rgb(QWidget):
+class rgbvideo(QWidget):
 
     def __init__(self, parent=None, thread=None):
-        super(rgb, self).__init__(parent)
-        loadUi('rgb.ui', self)
+        super(rgbvideo, self).__init__(parent)
+        loadUi('rgbvideo.ui', self)
         self.parent = parent
         self.thread = thread
 
-        # 사진파일 읽기
-        imgBGR = cv2.imread('lake_louise.jpg')
-        imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+        self.thread.changePixmap.connect(self.setImage)
+        self.thread.Frame.connect(self.setRGB)
 
-        B, G, R = cv2.split(imgBGR)
 
-        zeros = np.zeros((imgBGR.shape[0], imgBGR.shape[1]), np.uint8)
+    def setImage(self, image):
+        self.originCam.setPixmap(QPixmap.fromImage(image).scaled(self.originCam.size(), Qt.KeepAspectRatio))
+
+    def setRGB(self, frame):
+        R, G, B = cv2.split(frame)
+        zeros = np.zeros((frame.shape[0], frame.shape[1]), np.uint8)
 
         imgR = cv2.merge((R, zeros, zeros))
         imgG = cv2.merge((zeros, G, zeros))
@@ -30,20 +33,18 @@ class rgb(QWidget):
         imgRB = cv2.merge((R, zeros, B))
         imgGB = cv2.merge((zeros, G, B))
 
-        h, w, c = imgRGB.shape
+        h, w, c = frame.shape
 
-        qimg = QtGui.QImage(imgRGB.data, w, h, w*c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         Rimg = QtGui.QImage(imgR.data, w, h, w * c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         Gimg = QtGui.QImage(imgG.data, w, h, w * c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         Bimg = QtGui.QImage(imgB.data, w, h, w*c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         RGimg = QtGui.QImage(imgRG.data, w, h, w * c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         RBimg = QtGui.QImage(imgRB.data, w, h, w * c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
         GBimg = QtGui.QImage(imgGB.data, w, h, w * c, QtGui.QImage.Format_RGB888).scaled(640, 480, Qt.KeepAspectRatio)
-
-        self.originCam.setPixmap(QPixmap.fromImage(qimg).scaled(self.originCam.size(), Qt.KeepAspectRatio))
         self.lbR.setPixmap(QPixmap.fromImage(Rimg).scaled(self.lbR.size(), Qt.KeepAspectRatio))
         self.lbG.setPixmap(QPixmap.fromImage(Gimg).scaled(self.lbG.size(), Qt.KeepAspectRatio))
         self.lbB.setPixmap(QPixmap.fromImage(Bimg).scaled(self.lbR.size(), Qt.KeepAspectRatio))
         self.lbRG.setPixmap(QPixmap.fromImage(RGimg).scaled(self.lbRG.size(), Qt.KeepAspectRatio))
         self.lbRB.setPixmap(QPixmap.fromImage(RBimg).scaled(self.lbRB.size(), Qt.KeepAspectRatio))
         self.lbGB.setPixmap(QPixmap.fromImage(GBimg).scaled(self.lbGB.size(), Qt.KeepAspectRatio))
+
